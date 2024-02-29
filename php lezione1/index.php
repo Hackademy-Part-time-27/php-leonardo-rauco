36ef6d5d034b7cf890c69bea029dcf562b0c7dfe
leonardo566 <147409155+leonardo566@users.noreply.github.com>
2024-02-29 20:34:32 +0100

    Add files via upload

---
+++ php lezione1/index.php
@@ -0,0 +1,83 @@
+<?php
+// traccia 1
+//$nome = 'leonardo';
+//$is_student = true;
+//$float = 1.80;
+//$age = 23;
+//echo 'ciao sono ' . $nome . '' . ' sono uno studente' . ' ho ' . $age . ' anni ' . ' sono alto ' . $float . ' cm';
+
+//echo gettype($nome);
+//echo gettype($age);
+//var_dump ($is_student);
+//echo gettype($float);
+
+//$stringa = 'ciao';
+//const PAROLA ="ciao sono la costante";
+// echo PAROLA;
+
+
+
+//Traccia2 
+
+//$text1 = 'marco';
+//$text2 = 'hai';
+//$text3 = 'sete';
+//$text4 = '?';
+//$text5 = 'perchè';
+//$text6 = 'hai';
+//$text7 = 'bevuto';
+//$text8 = 'tutto';
+
+//echo $text1 . " " . $text2 . " " . $text3 . " " .$text4 ;
+
+//echo $text5 . " " . $text6 . " " . $text7 . " " . $text8;
+
+
+//$words1 =
+ [
+    'una',
+    67,
+    'vita',
+    'colle',
+    'mi',
+    'rosso',
+    [
+    'oscura',
+    'era',
+    89,
+    [
+    'mezzo',
+    [
+    'cammin',
+    'Nel',
+    [
+    'selva',
+    'la',
+    [
+    'via',
+    'una',
+    true,
+    ]
+    ],
+    ]
+    ],
+    'ritrovai',
+    'per'
+    ],
+    'diritta'
+    ];
+    $words2 = [
+    'elemento1' => 25.89,
+    'elemento2' => 'nostra',
+    'elemento3' => [
+    'Virgilio',
+    'smarrita',
+    'ché'
+    ]
+    ];
+//var_dump($words1);
+//print_r($words1);
+//$d = 'dì';
+//$resutl = $words1[6][3][1][1] . " " . $words1[6][3][0] . " del " . $words1[6][3][1][0] . "  " . $d . " " . $words2['elemento2'] . " " . $words1[2]. " " . $words1[4] . " ". $words1[6][4] . " " . $words1[0] . " " . $words1[6][3][1][2][0] . " " . $words1[6][0] . " " . $words2['elemento3'][2] . " " . $words1[6][3][1][2][1] . " " . $words1[7] . " " . $words1[6][3][1][2][2][0] . " " . $words1[6][1] . " " . $words2['elemento3'][1];
+ //echo $resutl;
+ 
---
+++ php lezione1/index2.php
@@ -0,0 +1,25 @@
+<?php
+//TRACCIA 1 ES:29/02
+
+//$users = [
+   // ['name' => 'Davide', 'surname' => 'Cariola', 'gender' => 'sig'],
+   // ];
+
+    //print_r($users);
+
+   // foreach($users as $elemento) {
+      //  echo " buongiorno" . " " . $elemento['gender'] . " " . $elemento['name'] . " " . $elemento['surname'] . "\n";
+    //}
+
+
+//TRACCIA 2
+ $rows = [1,2,3,4,5,6,7,8,9,10];
+ foreach ($rows as $riga){
+ if ($riga % 2 === 0){
+ echo "Mi trovo a riga \n";
+
+}
+}
+
+
+
